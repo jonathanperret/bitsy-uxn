54807d4a6893a06af3fb50dd81fa9b343fc6d084
Jonathan Perret <j-github@jonathanperret.net>
2022-01-11 21:17:16 +0100

    Basic avatar movement

--- uxnemurun.sh
+++ uxnemurun.sh
@@ -9,9 +9,9 @@ uxnasm $f ${f%.tal}.rom 2>&1 | grep -v 'Unused label'
 
 < /dev/null UXNEMU_TOP_LEFT=1 UXNEMU_ON_TOP=1 uxnemu -s 2 ${f%.tal}.rom "$@" &
 
-for i in {1..20}; do
-  osascript -l JavaScript -e 'iTerm = Application("iTerm2"); if(!iTerm.frontmost()) iTerm.activate()' > /dev/null
-  sleep .1
-done
-
+osascript -l JavaScript -e 'iTerm = Application("iTerm2");
+  while(iTerm.frontmost()) { delay(0.1); }
+  let i = 0;
+  while(!iTerm.frontmost() || (i++<2)) { iTerm.activate(); delay(0.1); }
+'
 wait
