a83ceb2bfc0ecb53154ca47580365eb6bd1ad364
Jonathan Perret <j-github@jonathanperret.net>
2022-01-11 21:37:49 +0100

    Make focus-keeping optional in run script

--- uxnemurun.sh
+++ uxnemurun.sh
@@ -9,9 +9,12 @@ uxnasm $f ${f%.tal}.rom 2>&1 | grep -v 'Unused label'
 
 < /dev/null UXNEMU_TOP_LEFT=1 UXNEMU_ON_TOP=1 uxnemu -s 2 ${f%.tal}.rom "$@" &
 
-osascript -l JavaScript -e 'iTerm = Application("iTerm2");
-  while(iTerm.frontmost()) { delay(0.1); }
-  let i = 0;
-  while(!iTerm.frontmost() || (i++<2)) { iTerm.activate(); delay(0.1); }
-'
+if [ "${KEEP_FOCUS:-}" = 1 ]; then
+  osascript -l JavaScript -e 'iTerm = Application("iTerm2");
+    while(iTerm.frontmost()) { delay(0.1); }
+    let i = 0;
+    while(!iTerm.frontmost() || (i++<2)) { iTerm.activate(); delay(0.1); }
+  '
+fi
+
 wait
